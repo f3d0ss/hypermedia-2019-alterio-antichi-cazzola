3870c7b8673f4f7f8cef47a6d06b787f3a7ca045
f-cazzola <federico.cazzola@mail.polimi.it>
2019-08-10 18:00:08 +0200

    add dropDB

--- util/create-database.js
+++ util/create-database.js
@@ -10,20 +10,29 @@ const promisePool = pool.promise();
 
 
 promisePool
-    .query("CREATE DATABASE festivalDB")
-    .then(() => {
-        console.log("Database created");
-        const pool = mysql.createPool({
-            host: 'localhost',
-            user: 'root',
-            database: 'festivalDB',
-            password: 'q1W@e3R$t5'
-        });
-        const promisePoolDb = pool.promise();
-        return promisePoolDb.query(`CREATE TABLE User (
-            id int PRIMARY KEY AUTO_INCREMENT,
-            email varchar(255) NOT NULL,
-            password varchar(255) NOT NULL
-        );`);
+    .query("DROP DATABASE festivalDB")
+    .then(() =>
+        createDatabase()
+    ).catch(err =>
+        createDatabase()
+    );
 
-    }).catch(err => console.log(err));
+function createDatabase() {
+    promisePool
+        .query("CREATE DATABASE festivalDB")
+        .then(() => {
+            console.log("Database created");
+            const pool = mysql.createPool({
+                host: 'localhost',
+                user: 'root',
+                database: 'festivalDB',
+                password: 'q1W@e3R$t5'
+            });
+            const promisePoolDb = pool.promise();
+            return promisePoolDb.query(`CREATE TABLE User (
+                id int PRIMARY KEY AUTO_INCREMENT,
+                email varchar(255) NOT NULL,
+                password varchar(255) NOT NULL
+            );`);
+        }).catch(err => console.log(err));
+}
