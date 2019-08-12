b046e31e7c294fb431dca4c8b3fb77ef64ae81fb
f-cazzola <federico.cazzola@mail.polimi.it>
2019-08-12 12:30:05 +0200

    refator database params

--- configuration/configuration.js
+++ configuration/configuration.js
@@ -1,4 +1,7 @@
-module.exports = 
-{
-    SECRET: "turangalella"
+module.exports = {
+    SECRET: "turangalella",
+    DB_PASSWORD: "q1W@e3R$t5",
+    DB_USER: "root",
+    DB_HOST: "localhost",
+    DB_NAME: "festivalDB"
 }
--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'festivalDB',
-    password: 'q1W@e3R$t5'
+    host: config.DB_HOST,
+    user: config.DB_USER,
+    password: config.DB_PASSWORD,
+    database: config.DB_NAME
 });
 
 module.exports = pool.promise();
