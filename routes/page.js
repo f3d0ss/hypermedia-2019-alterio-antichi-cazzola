658450886d1a33cd707341723d05595d3dbfb975
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-08-30 22:02:00 +0200

    Update

--- routes/page.js
+++ routes/page.js
@@ -6,5 +6,6 @@ router.get('/performer', (req, res, next) => res.render('performer'));
 router.get('/event', (req, res, next) => res.render('artisticEvent'));
 router.get('/login', (req, res, next) => res.render('login'));
 router.get('/registration', (req, res, next) => res.render('registration'));
+router.get('/performerGroup', (req, res, next) => res.render('performerGroup'));
 
 module.exports = router;
