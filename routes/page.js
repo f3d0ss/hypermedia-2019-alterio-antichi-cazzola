6d42aac1ac5d67371b4585b0a9b1fc2ba1be4cb8
ClarenceAntichi <48724542+ClarenceAntichi@users.noreply.github.com>
2019-09-01 17:06:29 +0200

    today page created

--- routes/page.js
+++ routes/page.js
@@ -7,5 +7,6 @@ router.get('/event', (req, res, next) => res.render('artisticEvent'));
 router.get('/login', (req, res, next) => res.render('login'));
 router.get('/registration', (req, res, next) => res.render('registration'));
 router.get('/contactUs', (req, res, next) => res.render('contactUs'));
+router.get('/today', (req, res, next) => res.render('today'));
 
 module.exports = router;
