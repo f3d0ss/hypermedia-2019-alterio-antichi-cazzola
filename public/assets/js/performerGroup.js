d26c556118277c1707cfd99c2033c751292e1278
f-cazzola <federico.cazzola@mail.polimi.it>
2019-09-03 16:25:02 +0200

    fix list link

--- public/assets/js/performerGroup.js
+++ public/assets/js/performerGroup.js
@@ -1,12 +1,11 @@
-const createCard = (container, name) =>
-{
+const createCard = (container, name, url) => {
     var card =
-            `
+        `
                 <div class="d-flex flex-row justify-content-center">
                     <div class="wrapper">
                         <div class="row">
                             <img class="performer-img" src="/images/HarryPotter.jpg" alt="First Artist">
-                            <h2 class="performer-name"><a class="performer-link" href="#">${name}</a></h2>
+                            <h2 class="performer-name"><a class="performer-link" href="${url}">${name}</a></h2>
                         </div>
                     </div>
                 </div>
@@ -14,34 +13,36 @@ const createCard = (container, name) =>
     container.innerHTML += card;
 }
 
-const onPerformerGroupLoad = async () =>
-{
-    try
-    {
+const onPerformerGroupLoad = async () => {
+    try {
         var title;
         var performers;
 
-        if(UrlLastPart === "performers")
-        {
+        if (UrlLastPart === "performers") {
             title = "All performers";
             performers = (await get(URLS.PERFORMER, 100)).response;
-        }
-        else
-        {
+        } else {
             title = "All companies";
             performers = (await get(URLS.COMPANY, 100)).response;
         }
         await createCards(title, performers);
+    } catch (e) {
+        console.log(e);
     }
-    catch(e) { console.log(e); }
 }
 
-const createCards = async (title, performers) =>
-{
+const createCards = async (title, performers) => {
     const performersContainer = byId("performersContainer");
     byId("title").innerHTML = byId("h1Title").innerHTML = byId("breadCrumbName").innerHTML = title;
-    for(var i=0; i < performers.length; i++)
-        createCard(performersContainer, performers[i].name)
+    let url;
+    for (var i = 0; i < performers.length; i++) {
+        if (performers[i].company_id) {
+            url = "performers/";
+        } else {
+            url = "companies/";
+        }
+        createCard(performersContainer, performers[i].name, url + performers[i].id);
+    }
 }
 
 onPerformerGroupLoad();
