4b1e35b38ff119a110530073ae117111cd4888ae
fede.alterio <alterio.federico@mail.polimi.it>
2019-09-03 10:18:23 +0200

    artistic event

--- public/assets/js/artisticEvent.js
+++ public/assets/js/artisticEvent.js
@@ -1,12 +1,12 @@
-const addFeaturing = (container, performer) =>
+const addIcon = (container, name, link) =>
 {
     var performerHtml = 
             `
                 <div class="artist-box">
-                    <a href="#">
+                    <a href="${link}">
                         <img class="artist-img" src="/images/HarryPotter.jpg">
                     </a>
-                    <p class="wheat-item a-name">${performer.name}</p>
+                    <p class="wheat-item a-name">${name}</p>
                 </div>
                 `;
     container.innerHTML += performerHtml;
@@ -24,14 +24,22 @@ const onArtisticEventLoad = async () =>
 const createFeaturings = async () =>
 {
     const featuringContainer = document.getElementById("featuringBox");
+    const eventsContainer = document.getElementById("eventsContainer");
     const abstract = document.getElementById("abstract");
     const id = location.href.substring(location.href.lastIndexOf('/') + 1);
 
     var performers = (await get(URLS.PERFORMER + "/event/" + id, 100)).response;
     var event = (await get(URLS.EVENT + "/" + id, 1)).response;
 
+    const date = event.date.substring(0,10);
+    var eventsSameDate = (await get(URLS.EVENT + "/date/" + date, 100)).response;
+
+    console.log(date);
+    console.log(eventsSameDate);
     for(var i=0; i < performers.length; i++)
-        addFeaturing(featuringContainer, performers[i]);
+        addIcon(featuringContainer, performers[i].name, "#");
+    for(var i=0; i < eventsSameDate.length; i++)
+        addIcon(eventsContainer, eventsSameDate[i].name, "#");
     abstract.innerHTML = event.abstract;
 }
 
