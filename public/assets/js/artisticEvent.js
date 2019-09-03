0a406732055c7b0dc2d272947e3df574b3b76019
fede.alterio <alterio.federico@mail.polimi.it>
2019-09-03 16:05:10 +0200

    reservation works

--- public/assets/js/artisticEvent.js
+++ public/assets/js/artisticEvent.js
@@ -12,6 +12,28 @@ const addIcon = (container, name, link) =>
     container.innerHTML += performerHtml;
 }
 
+const addBookBtn = () =>
+{
+    var buttonHTML = 
+        `
+            <div class="col text-center">
+              <button id="bookBtn" type="button" class="btn btn-dark">Book to the Event!</button>
+            </div>
+        `;
+    byId("bottomContainer").innerHTML = buttonHTML;
+}
+
+const addReservedLabel = () =>
+{
+    var reservedHTML = 
+                `
+                    <div class="col text-center">
+                        <h1><span class="badge badge-info">RESERVED</span></h1>
+                    </div>
+                `;
+    byId("bottomContainer").innerHTML = reservedHTML;
+}
+
 const onArtisticEventLoad = async () =>
 {
     try
@@ -20,7 +42,7 @@ const onArtisticEventLoad = async () =>
         const event = (await get(URLS.EVENT + "/" + id, 1)).response;
         const seminar = (await get(URLS.SEMINAR + "/" + event.seminar_id)).response;
         createIcons(event);
-        setupBookButton(id);
+        setupBottomPage(id);
         abstract.innerHTML = event.abstract;
         byId("title").innerHTML = event.name;
         byId("breadCrumbName").innerHTML = event.name;
@@ -48,12 +70,32 @@ const createIcons = async event =>
         addIcon(eventsContainer, eventsSameDate[i].name, "/events/" + eventsSameDate[i].id);
  }
 
-const bookTheEvent = id =>
+const setupBottomPage = async id =>
 {
-    post(URLS.RESERVATION, 
+    if(token)
+    {
+        const reservations = (await get(URLS.RESERVATION + "/user/" + userID, 1)).response;
+        console.log(reservations, id);
+    
+        for(var i=0; i < reservations.length; i++)
+            if(reservations[i].event_id == id)
+            {
+                addReservedLabel();
+                return;
+            }
+    }
+
+    addBookBtn();
+    setupBookButton(id);
+}
+
+const bookTheEvent = async id =>
+{
+    await post(URLS.RESERVATION, 
         {
             "eventId": id
         }, true)
+    location.reload();
 }
 
 const setupBookButton = async id =>
